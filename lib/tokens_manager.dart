4d0fac292aed1b11f50e3d7721c63a08c91b80a6
alibek.g <brotherf.dev@gmail.com>
2023-03-08 18:07:27 +0300

    remove fresh

--- lib/tokens_manager.dart
+++ lib/tokens_manager.dart
@@ -6,5 +6,3 @@ export 'src/interceptors/fresh_interceptor.dart';
 export 'src/enums/enums.dart';
 export 'src/interceptors/interceptors.dart';
 export 'src/storages/storages.dart';
-
-export 'package:fresh_dio/fresh_dio.dart';
