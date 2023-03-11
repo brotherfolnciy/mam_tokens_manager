2e80351d746889eb6f858ecf156c9661f305a4b0
alibek.g <brotherf.dev@gmail.com>
2023-03-11 12:49:08 +0300

    returned models path to export

--- lib/tokens_manager.dart
+++ lib/tokens_manager.dart
@@ -4,3 +4,4 @@ export 'src/tokens_manager.dart';
 export 'src/interceptors/fresh_interceptor.dart';
 export 'src/enums/enums.dart';
 export 'src/interceptors/interceptors.dart';
+export 'src/models/models.dart';
