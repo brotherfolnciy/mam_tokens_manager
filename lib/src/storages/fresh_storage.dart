c4f6fd4dad4933559c67ae21969998e1fa81dfda
alibek.g <brotherf.dev@gmail.com>
2023-03-11 21:59:20 +0300

    refactored fresh storage

--- lib/src/storages/fresh_storage.dart
+++ lib/src/storages/fresh_storage.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:fresh_dio/fresh_dio.dart';
 import 'package:logger/logger.dart';
-import 'package:tokens_manager/src/models/tokens.dart';
 import 'package:tokens_manager/tokens_manager.dart';
 
 const FlutterSecureStorage _flutterSecureStorage = FlutterSecureStorage();
@@ -22,11 +20,6 @@ class TokensStorage extends TokenStorage<Tokens> {
   // Это поле служит только для форматирования о отображения логов
   // об изменении refresh токена, не использовать в иных операциях
   String? _refreshToken;
-
-  void setMockInitialValues(Map<String, String> values) {
-    // ignore: invalid_use_of_visible_for_testing_member
-    FlutterSecureStorage.setMockInitialValues(values);
-  }
 
   @override
   Future<void> delete() async {
