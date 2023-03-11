a467a93da2462e469d3157aaa1c7575baf4b322e
alibek.g <brotherf.dev@gmail.com>
2023-03-11 21:59:27 +0300

    refactored barrels

--- lib/tokens_manager.dart
+++ lib/tokens_manager.dart
@@ -1,8 +1,6 @@
 library tokens_manager;
 
 export 'src/tokens_manager.dart';
-export 'src/enums/enums.dart';
-export 'src/interceptors/interceptors.dart';
 export 'src/models/models.dart';
 
 export 'package:fresh_dio/fresh_dio.dart';
